d3e04bfb5b297b5375f96d0477c0a2b5890587e4
David <dpez991@gmail.com>
2026-04-12 02:21:12 -0600

    añadir archivos para que el resto trabaje

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '159b3b2f66a393753ec4ed0e13fd6eea1eef2271',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '159b3b2f66a393753ec4ed0e13fd6eea1eef2271',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
