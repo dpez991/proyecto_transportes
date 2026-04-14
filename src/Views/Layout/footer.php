390ef96349fac58a9bfbaddd0223251d98b5d83f
jorgep1988 <jbeto1397@gmail.com>
2026-04-13 21:46:24 -0600

    Add files via upload

---
+++ src/Views/Layout/footer.php
@@ -0,0 +1,36 @@
+<footer class="bg-dark text-white pt-4 pb-2 mt-5">
+    <div class="container text-center text-md-start">
+        <div class="row">
+            <div class="col-md-4 col-lg-4 col-xl-3 mx-auto mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">Transporte VIP</h6>
+                <p>
+                    Tu solución confiable para viajes seguros y cómodos. 
+                    Conectando destinos, uniendo personas.
+                </p>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">Enlaces</h6>
+                <p><a href="index.php?page=home" class="text-reset text-decoration-none">Inicio</a></p>
+                <p><a href="index.php?page=about" class="text-reset text-decoration-none">Empresa</a></p>
+                <p><a href="index.php?page=contact" class="text-reset text-decoration-none">Contacto</a></p>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">Contacto</h6>
+                <p>Ciudad, País</p>
+                <p>info@transporte.com</p>
+                <p>+504 1234-5678</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center p-3 border-top border-secondary">
+        © <?php echo date("Y"); ?> Copyright:
+        <a class="text-reset fw-bold" href="#">MiTransporte.com</a>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
